f24f3197700566fef443cf16997dae6dc83ede60
philipuuu <topgun0415@naver.com>
2021-10-09 20:14:06 +0900

    test: github update

--- pages/detail/detail.tsx
+++ pages/detail/detail.tsx
@@ -1,5 +1,11 @@
 const Detail = (): JSX.Element => {
-  return <div>성계님 작업 여기</div>;
+  const num = 1;
+  console.log(num);
+  return (
+    <div>
+      <div>성계님 작업 여기</div>
+    </div>
+  );
 };
 
 export default Detail;
