db21af95072d045aea6ef2c44dd77473d4bd86a8
Alice Kim <aliceinkorea.frontend@gmail.com>
2021-10-10 20:31:20 +0900

    Refactor: mui 패키지 수정 및 mui로 공용 레이아웃 적용

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,5 +1,22 @@
+import * as React from 'react';
+import { AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
 const Navbar: React.FC = () => {
-  return <nav>nav bar</nav>;
+  return (
+    <Box sx={{ flexGrow: 1, position: 'sticky' }}>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Logo
+          </Typography>
+          <IconButton size="large" edge="end" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,98 @@
 import type { NextPage } from 'next';
+import { Typography, Card, CardActions, CardContent, Grid, Container, Button } from '@mui/material';
+import useStyles from 'src/styles/mui/main';
 
 const Home: NextPage = () => {
-  return <div>main</div>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <main>
+        <div>
+          <Container maxWidth="sm" style={{ marginTop: '20px' }}>
+            <Typography variant="h3" align="center" color="textPrimary" gutterBottom>
+              The real job for you, R9
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente unde tempora
+            </Typography>
+            <div>
+              <Grid container spacing={2} justifyContent="center" style={{ marginTop: '20px' }}>
+                <Grid item>
+                  <Button variant="outlined" color="primary" size="small">
+                    #안양
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary" size="small">
+                    #청소
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid}>
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="h5" gutterBottom>
+                    Heading
+                  </Typography>
+                  <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam iste a deleniti
+                    quasi facere doloremque illo non provident sequi placeat?
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    view
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="h5" gutterBottom>
+                    Heading
+                  </Typography>
+                  <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam iste a deleniti
+                    quasi facere doloremque illo non provident sequi placeat?
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    view
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="h5" gutterBottom>
+                    Heading
+                  </Typography>
+                  <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam iste a deleniti
+                    quasi facere doloremque illo non provident sequi placeat?
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    view
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default Home;
