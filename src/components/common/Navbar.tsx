15cb9dfea5e0ba5b69c52ed9d410e8786392be1c
Alice Kim <aliceinkorea.frontend@gmail.com>
2021-10-13 00:31:56 +0900

    Feat: side bar 추가

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,16 +1,30 @@
 import * as React from 'react';
+import { useRouter } from 'next/router';
 import { AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
-const Navbar: React.FC = () => {
+const Navbar = ({ toggleDrawer }: any): JSX.Element => {
+  const router = useRouter();
+
   return (
     <Box sx={{ flexGrow: 1, position: 'sticky' }}>
       <AppBar>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            onClick={() => router.push('/')}
+          >
             Logo
           </Typography>
-          <IconButton size="large" edge="end" color="inherit" aria-label="menu">
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+          >
             <MenuIcon />
           </IconButton>
         </Toolbar>
