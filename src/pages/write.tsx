010ed744c6e7be2eaa2b4ec014c696f7b2081034
이지호 <cctv1998@naver.com>
2021-10-31 16:49:58 +0900

    Feat : 공고 등록 페이지

---
+++ src/pages/write.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PostEditor from "src/components/write/PostEditor";
+import dynamic from "next/dynamic";
+
+
+
+const write = () => {
+  return (
+    <>
+      <PostEditor />
+    </>
+  );
+};
+
+export default write;
