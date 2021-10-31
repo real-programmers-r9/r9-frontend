fe476143bda28c5e99667587c857aa3011ccbcfb
leejiho9898 <84016039+leejiho9898@users.noreply.github.com>
2021-10-31 22:55:03 +0900

    Design : postWrite page  (#18)
    * Feat : useModal hook
    
    * Feat : 공고 등록 페이지
    
    * Style : postEditor
    
    * Refector : useDaumAdress 훅 분리
    
    * Add : daum post code
    
    Co-authored-by: Jongho Hong <kr.myodan@gmail.com>

---
+++ src/pages/write.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PostEditor from "src/components/write/PostEditor";
+
+
+
+const write = () => {
+  return (
+    <>
+      <PostEditor />
+    </>
+  );
+};
+
+export default write;
