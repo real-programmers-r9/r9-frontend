c889a322492836f911b2c9bc3639ffce6a60fff8
philipuuu <topgun0415@naver.com>
2021-10-31 18:39:00 +0900

    Style : Typography & Favorite MUI styled

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,5 +1,40 @@
+import * as React from "react";
+import { Typography, Box, Rating } from "@mui/material";
+import { FavoriteBorderOutlined } from "@mui/icons-material";
+
 const Detail: React.FC = () => {
-  return <div>성계님 작업 여기</div>;
+  return (
+    <div>
+      <div className="TitleBar">
+        <br />
+        <Typography
+          variant="h5"
+          gutterBottom
+          component="div"
+          textAlign="center"
+          fontWeight="bold"
+        >
+          강남역 도야짬뽕 홀서빙
+          <FavoriteBorderOutlined
+            sx={{ position: "absolute", right: 30, height: "1.3em" }}
+          />
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
+
+/* <div className: Ratingbar> 
+        <Box align="left" component="fieldset" mb={3} borderColor="transparent">
+          <Rating
+            value={value}
+            name="rating"
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            onClick={props.handleInputChange}
+          />
+        </Box>
+      </div> */
