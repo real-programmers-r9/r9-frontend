480cf406bb1cc44a04c48fa439db3e951f0e28cc
이지호 <cctv1998@naver.com>
2021-11-06 17:18:18 +0900

    Feat : job Type

--- src/types/stores.ts
+++ src/types/stores.ts
@@ -1,3 +1,5 @@
+import { JobStatus, PayMentsMethod } from "./enums";
+
 export interface User {
   createdAt: Date;
   updatedAt: Date;
@@ -10,4 +12,21 @@ export interface User {
 
 export interface Auth {
   user: User | null;
+}
+
+export interface IJobState {
+  writer: User | null;
+  title: string;
+  deadline: Date;
+  detail: string;
+  personnel: number;
+  age: number;
+  workType: string;
+  adress: string;
+  payment: PayMentsMethod;
+  workingDay: string[];
+  startTime: Date;
+  endTime: Date;
+  wage: number;
+  status: JobStatus;
 }
