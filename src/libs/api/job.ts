7785d885654a85f6a98b0ceef3f26fb9ad831d87
이지호 <cctv1998@naver.com>
2021-11-07 18:00:19 +0900

    Feat : job api

---
+++ src/libs/api/job.ts
@@ -0,0 +1,30 @@
+import { IJobState } from "src/types/stores";
+import client from "./client";
+
+/** 공고 생성 */
+export const createJobAPI = async (job: IJobState) => {
+  const response = await client.post("/jobs", job);
+  return response.data;
+};
+
+/** 공고 리스트 호출*/
+export const findJobsAPI = async () => {
+  const resposne = await client.get("/jobs");
+  return resposne.data;
+};
+
+/** 특정 ID 가진 공고 호출 */
+export const findJobByIdAPI = async (id: number) => {
+  const resposne = await client.get(`/jobs/${id}`);
+  return resposne.data;
+};
+
+/** 공고 삭제 */
+export const deleteJobAPI = async (id: number) => {
+  await client.delete(`/jobs/${id}`);
+};
+
+/** 공고 업데이트 */
+export const updateJobAPI = async (id: number) => {
+  await client.patch(`/jobs/${id}`);
+};
