aad008724b442b3df43e80deae2fd5384ddef4b2
이지호 <cctv1998@naver.com>
2021-11-11 02:39:43 +0900

    Refector : hook url 수정

---
+++ src/hooks/reviews/useReviewEffect.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { findReviewsAPI } from "src/libs/api/review";
+
+export default function useReviewEffect() {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const reviews = await findReviewsAPI();
+        setReviews(reviews);
+      } catch (error) {
+        alert("스케쥴 목록을 불러오는데 실패했습니다.");
+      }
+    };
+    getData();
+  }, [setReviews]);
+
+  return {reviews, setReviews};
+}
