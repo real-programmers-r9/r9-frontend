263ea378e52118fc4207a82acd195c49a9887203
Jongho Hong <kr.myodan@gmail.com>
2021-11-10 23:03:18 +0900

    Update api.ts

--- src/redux/services/api.ts
+++ src/redux/services/api.ts
@@ -6,6 +6,7 @@ import { User } from "~/types/user";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
+    // http://r9-backend.herokuapp.com
     baseUrl: "http://localhost:4000",
     credentials: "include",
   }),
