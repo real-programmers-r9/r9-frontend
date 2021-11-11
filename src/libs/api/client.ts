05b1aae3c942c639628f2fbcc719566de5cfc470
Jongho Hong <kr.myodan@gmail.com>
2021-11-11 18:25:46 +0900

    Feat: Base url env

--- src/libs/api/client.ts
+++ src/libs/api/client.ts
@@ -4,7 +4,6 @@ const client = axios.create({
   withCredentials: true, // 쿠키주고받는거
 });
 
-// http://r9-backend.herokuapp.com
-client.defaults.baseURL = "http://localhost:4000";
+client.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_BASEURL;
 
 export default client;
