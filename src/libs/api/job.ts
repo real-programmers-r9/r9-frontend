80e2c02d71a9ecb9729d5e769f5ca8fce0697382
이지호 <cctv1998@naver.com>
2021-11-20 19:34:49 +0900

    Feat : API 알아보기 쉽게 수정

--- src/libs/api/job.ts
+++ src/libs/api/job.ts
@@ -3,34 +3,33 @@ import { IJobState } from "src/types/stores";
 import client from "./client";
 
 const url = {
-  GET_JOBS: "/jobs",
-  GET_JOBID:""
+  JOBS: "/jobs",
 };
 
 /** 공고 생성 */
 export const createJobAPI = async (job: IJobState) => {
-  const response = await client.post("/jobs", job);
+  const response = await client.post(url.JOBS, job);
   return response.data;
 };
 
 /** 공고 리스트 호출 */
 export const findJobsAPI = async () => {
-  const resposne = await axios.get(url.GET_JOBS);
+  const resposne = await axios.get(url.JOBS);
   return resposne.data;
 };
 
 /** 특정 ID 가진 공고 호출 */
-export const findJobByIdAPI = async (id: number) => {
-  const resposne = await client.get(`${url.GET_JOBS}/${id}`);
+export const findJobByIdAPI = async (id: any) => {
+  const resposne = await client.get(`${url.JOBS}/${id}`);
   return resposne.data;
 };
 
 /** 공고 삭제 */
 export const deleteJobAPI = async (id: number) => {
-  await client.delete(`/jobs/${id}`);
+  await client.delete(`${url.JOBS}/${id}`);
 };
 
 /** 공고 업데이트 */
 export const updateJobAPI = async (id: number) => {
-  await client.patch(`/jobs/${id}`);
+  await client.patch(`${url.JOBS}/${id}`);
 };
